be51c468f66ab29aa01340098dbd46bf05a0b3d9
Sahril <103849124+CallMeSahril@users.noreply.github.com>
2025-07-16 19:22:59 +0700

    add e

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ class Config:
     MYSQL_HOST = '127.0.0.1'
     MYSQL_USER = 'flask_user'
     MYSQL_PASSWORD = 'passwordku123'
-    MYSQL_DB = 'eduxplore_db'
+    MYSQL_DB = 'eduexplore_db'
     MYSQL_CURSORCLASS = 'DictCursor'
