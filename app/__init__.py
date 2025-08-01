f0528f550d4e36c6af185fd0e610804f4509abe6
Sahril <103849124+CallMeSahril@users.noreply.github.com>
2025-08-01 07:18:10 +0700

    .

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,12 @@ from app.routes.api import api_blueprint
 from app.config import Config
 from app.extensions.db import mysql
 from app.routes.soal_routes import soal_bp
+from app.routes.user_routes import user_bp
+
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = 'ini_rahasia_sangat_rahasia_123' 
+    app.secret_key = 'ini_rahasia_sangat_rahasia_123'
     CORS(app)
 
     # Load config
@@ -20,5 +22,6 @@ def create_app():
     # Register API Blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api')
     app.register_blueprint(soal_bp)
+    app.register_blueprint(user_bp)
 
     return app
--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,24 +1,24 @@
-from flask_restx import Namespace, Resource
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from app.services.user_service import get_user_profile, delete_user
 from app.services.user_service import change_user_password
 from flask import request  # ← tambahkan ini
 
-api = Namespace('user', description='User operations')
+user_bp = Blueprint('user', description='User')
 
 
-@api.route('/profile/<int:user_id>')
+@user_bp.route('/profile/<int:user_id>')
 class UserProfile(Resource):
     def get(self, user_id):
         return get_user_profile(user_id)
 
 
-@api.route('/<int:user_id>')
+@user_bp.route('/<int:user_id>')
 class UserDelete(Resource):
     def delete(self, user_id):
         return delete_user(user_id)
 
 
-@api.route('/change-password/<int:user_id>')
+@user_bp.route('/change-password/<int:user_id>')
 class ChangePassword(Resource):
     def put(self, user_id):
         data = request.get_json()
