f2cb2dd12ed661dce78352f57561e3ad00cf3463
Sahril <103849124+CallMeSahril@users.noreply.github.com>
2025-08-01 07:20:13 +0700

    .

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ from app.routes.api import api_blueprint
 from app.config import Config
 from app.extensions.db import mysql
 from app.routes.soal_routes import soal_bp
-from app.routes.user_routes import user_bp
 
 
 def create_app():
@@ -22,6 +21,5 @@ def create_app():
     # Register API Blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api')
     app.register_blueprint(soal_bp)
-    app.register_blueprint(user_bp)
 
     return app
