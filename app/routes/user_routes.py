1b107ebef8db1d83d177857e6effbe4cce591151
Sahril <103849124+CallMeSahril@users.noreply.github.com>
2025-08-01 07:19:50 +0700

    .

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,24 +1,27 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
-from app.services.user_service import get_user_profile, delete_user
-from app.services.user_service import change_user_password
-from flask import request  # ← tambahkan ini
+from flask_restx import Namespace, Resource
+from flask import request
+from app.services.user_service import (
+    get_user_profile,
+    delete_user,
+    change_user_password
+)
 
-user_bp = Blueprint('user', __name__)
+api = Namespace('user', description='User operations')
 
 
-@user_bp.route('/profile/<int:user_id>')
+@api.route('/profile/<int:user_id>')
 class UserProfile(Resource):
     def get(self, user_id):
         return get_user_profile(user_id)
 
 
-@user_bp.route('/<int:user_id>')
+@api.route('/<int:user_id>')
 class UserDelete(Resource):
     def delete(self, user_id):
         return delete_user(user_id)
 
 
-@user_bp.route('/change-password/<int:user_id>')
+@api.route('/change-password/<int:user_id>')
 class ChangePassword(Resource):
     def put(self, user_id):
         data = request.get_json()
